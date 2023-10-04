88b112a0cc051a96b3ff361162bf5f9e4b5ef1b5
lekshmypk <pklekshmi96@gmail.com>
2023-10-04 22:22:22 +0530

    update third party module for second time

---
+++ eCourt/resources/views/thirdparty/viewcasereports.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.profiletheme')
+@section('content')
+
+<h4 style="color:black; padding-left:16%; text-decoration:underline; padding: left 60%;">Thirdparty Dashboard</h4>
+<iframe src="{{asset('storage/ecourt/case_reports/casereport1.jpeg')}}" style=" width: 1400px;; height: 500px; margin:2%;"frameborder="5">
+</iframe>
+
+@endsection
